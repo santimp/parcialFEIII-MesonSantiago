4c47bbb5bdf44d1390697c5c4d540bf22091dc69
santimp <santimeson@gmail.com>
2023-08-15 07:22:43 -0300

    first commit

---
+++ parcialfeIII/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card  from './components/Card'
+
+function App() {
+
+  return (
+    <>
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ parcialfeIII/src/components/Card.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Card.css'
+
+const Card = () => {
+  return ( <div className='card'> soy una tarjeta </div> )
+}
+
+export default Card
